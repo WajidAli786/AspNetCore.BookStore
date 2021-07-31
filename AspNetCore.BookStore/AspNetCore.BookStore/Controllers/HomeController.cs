161f02af9b56f44ece33ec5afb510c57dc6ca85f
WajidAli786 <56424115+WajidAli786@users.noreply.github.com>
2021-07-31 07:14:09 +0500

    added mvc

---
+++ AspNetCore.BookStore/AspNetCore.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string index()
+        {
+            return "Controller";
+        }
+    }
+}
