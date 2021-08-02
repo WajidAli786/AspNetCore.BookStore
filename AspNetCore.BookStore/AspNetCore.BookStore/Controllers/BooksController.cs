891bba756a093cc1bb57242e8441f3cf6505d0ec
WajidAli786 <56424115+WajidAli786@users.noreply.github.com>
2021-08-02 08:28:25 +0500

    added view_start file

--- AspNetCore.BookStore/AspNetCore.BookStore/Controllers/BooksController.cs
+++ AspNetCore.BookStore/AspNetCore.BookStore/Controllers/BooksController.cs
@@ -1,16 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore.BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCore.BookStore.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly BookRepository _bookRepository = null;
+
+        public BooksController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult GetAllBooks()
         {
-            return View();
+            var data = _bookRepository.GetAllBooks();
+            return View(model: data);
+        }
+
+        public IActionResult GetBook(int id)
+        {
+            var data = _bookRepository.GetBookById(bookId: id);
+            return View(model: data);
         }
     }
 }
--- AspNetCore.BookStore/AspNetCore.BookStore/Repository/BookRepository.cs
+++ AspNetCore.BookStore/AspNetCore.BookStore/Repository/BookRepository.cs
@@ -1,23 +1,36 @@
 ﻿using AspNetCore.BookStore.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AspNetCore.BookStore.Repository
 {
     public class BookRepository
     {
-        //public List<> GetAllBooks()
-        //{
+        public List<Book> GetAllBooks()
+        {
+            return DataSource();
+        }
 
-        //}
+        public Book GetBookById(int bookId)
+        {
+            return DataSource().Where(x => x.Id == bookId).FirstOrDefault();
+        }
 
-        //public Book GetBookById(int id)
-        //{
+        public List<Book> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+        }
 
-        //}
-
-        //public List<Book> SearchBook(string titlel)
-        //{
-
-        //}
+        private List<Book> DataSource()
+        {
+            return new List<Book>()
+            {
+                new Book(){Id =1, Title="MVC", Author = "Nitish", Description = "This descrition for MVC book", Category="Programming", Language="English", TotalPages=1000},
+                new Book(){Id =2, Title="Dot Net Core", Author = "Nitish", Description = "This descrition for .NET CORE book" , Category="Programming", Language="English", TotalPages=1000},
+                new Book(){Id =3, Title="C#", Author = "Monika", Description = "This descrition for C# book" , Category="Programming", Language="English", TotalPages=1000},
+                new Book(){Id =4, Title="Java", Author = "Webgentle", Description = "This descrition for JAVA book" , Category="Programming", Language="English", TotalPages=1000},
+                new Book(){Id =5, Title="Php", Author = "Webgentle", Description = "This descrition for PHP book" , Category="Programming", Language="English", TotalPages=1000},
+            };
+        }
     }
 }
